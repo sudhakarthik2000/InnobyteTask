9af1ae76c25bf079da1aa1c7450f4100b0915bbd
sudhakarthik2000 <sudhakarthik2000@gmail.com>
2024-09-18 10:49:34 +0530

    First

---
+++ talwindconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1a202c',
+          secondary: '#2d3748',
+          accent: '#4a5568',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
